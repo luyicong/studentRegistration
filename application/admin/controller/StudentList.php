511ce24299acc6b7d889440044f20620cc9c1dfc
lyc123 <980469887@qq.com>
2018-02-24 15:47:53 +0800

    。。。。

---
+++ application/admin/controller/StudentList.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhujia100_L
+ * Date: 2018/2/23
+ * Time: 16:27
+ */
+
+namespace app\admin\controller;
+
+
+use think\Controller;
+
+class StudentList extends Controller
+{
+    //职位首页/职位列表
+    public function index() {
+
+//        $result = model('StudentList')->getPosListForAdmin();
+//
+//        $this->assign('posList',$result);
+
+        $list = db('user')->select();
+
+        $this->assign('list',$list);
+
+        return $this->fetch();
+    }
+
+    //编辑信息
+    public function edit(){
+
+        return $this->fetch();
+    }
+
+    //预览学生报名信息
+
+    public function preview() {
+
+        return $this->fetch();
+    }
+}
